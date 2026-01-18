6979f2f98bc89605381a1467f0808f2f5ddccadd
Klauss Tattoo <klauss@gmail.com>
2026-01-18 18:25:56 -0300

    Mi primer Commit

---
+++ Clase02/primerCodigo.py
@@ -0,0 +1,12 @@
+#Iniciamos Con Codigo... ASI SE COMENTA
+
+Name1 ="Claudio "
+Name2 ="Tarsia "
+Edad = 44 
+Pais = "Argentina " 
+Ciudad ="Buenos Aires\nCABA "
+
+print (Name1,Name2, )
+print (Edad, Pais, )
+print(Ciudad)
+
