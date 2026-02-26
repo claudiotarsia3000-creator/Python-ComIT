a5397cc313a3ec17b8d8f29b344603ac04cba458
ClaudioTarsia <claudiotarsia3000@gmail.com>
2026-02-26 18:50:46 -0300

    Algunas modificaciones para perfeccionar el codigo

--- Clase17/Persona.py
+++ Clase17/Persona.py
@@ -1,38 +1,18 @@
-# class Persona:
-#     pass
 
-# persona1 = Persona()
-# persona2 = Persona()    
-
-# print(type(persona1))
-# print(persona1 == persona2)   
-
-# persona3 = Persona()
-# persona3.nombre = "Ana"
-# persona3.edad = 30
-# print(persona3.nombre)
-# print(persona3.edad)        
-
-# p1 = Persona()
-# p1.nombre = "Juan"
-
-# p2 = Persona()
-# p2.nombre = "Maria"
-
-# print(p1.nombre)
-# print(p2.nombre)    
-
+# Persona.py - Ejercicio de POO Clase 17
+# En este ejercicio aprendimos a crear una clase y usar el constructor __init__
 
 class Persona:
     def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
 
+# Pruebas manuales (como hicimos en clase)
 persona1 = Persona("Juan", 30)
 print(persona1.nombre)
 print(persona1.edad)
 
 persona2 = Persona("Carlos", 25)
-print(persona2) 
+print("Nombre de la segunda persona:", persona2.nombre)
 
  
--- Clase17/ej3.py
+++ Clase17/ej3.py
@@ -1,15 +1,17 @@
 class Casa:
-    def __init__(self, num_cuartos, num_baños, num_pisos):
+    def __init__(self, num_cuartos, num_baños, num_pisos, tipo_de_vivienda):
         self.num_cuartos = num_cuartos
         self.num_baños = num_baños
         self.num_pisos = num_pisos
+        self.tipo_de_vivienda = tipo_de_vivienda # Corregido: ahora guarda el texto
 
+# Todas las casas DEBEN tener los 4 datos que pide el __init__
+casa1 = Casa(3, 4, 2, 'Casa Pequeña')
+casa2 = Casa(4, 2, 2, 'Departamento') # Agregué el tipo
+casa3 = Casa(3, 2, 1, 'Cabaña')        # Agregué el tipo
 
-casa1 = Casa (3,4,1)
-casa2 = Casa (4,2,2)
-casa3 = Casa (3,2,1)
-casaGrande = Casa (4,3,2)
-casaGrande2 = Casa (5,3,3)
-casaGigante = Casa (5,4,3)
-casaGiante2 = Casa (6,4,4)
+# Corregimos el print para que use los atributos
+print(f'La propiedad es una {casa1.tipo_de_vivienda} de {casa1.num_cuartos} cuartos, {casa1.num_baños} baños y {casa1.num_pisos} pisos.')
+print(f'Esta propiedad es un {casa2.tipo_de_vivienda} de {casa2.num_cuartos} cuartos, {casa2.num_baños} baños y {casa2.num_pisos} pisos.') 
+print(f'Este inmueble, es una {casa3.tipo_de_vivienda}, Cuenta con {casa3.num_cuartos} cuartos, con {casa3.num_baños} baños y esta establecida en {casa3.num_pisos} pisos')
 
